18343ed59508fa6014906e22f3ae450b3c743123
Mahendra <mkhati47@gmail.com>
2021-06-24 21:00:19 +0530

    fixed header

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import {
-    ImageBackground
+    View,
 } from 'react-native';
 import DrawerNavigator from './navigation/drawerNavigator';
 import { globalStyles } from './styles/global';
 
 export default function App() {
     return (
-        <ImageBackground style={globalStyles.main}>
+        <View style={globalStyles.main}>
             <DrawerNavigator />
-        </ImageBackground>
+        </View>
     );
 };
